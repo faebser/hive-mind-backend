a8572e7ad4cc463ca95019833065b963fda5747d
faebser <fabian@esf-frei.ch>
2019-12-25 14:59:23 +0100

    Fixed port in prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :hive_backend, HiveBackendWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  http: [:inet6, port: 8083],
+  url: [host: "tofu.wtf", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
